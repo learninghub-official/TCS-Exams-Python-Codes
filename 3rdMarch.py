367787f8c61ef71a689178db7aef244f666ae53d
Mayank Nagar <71215472+learninghub-official@users.noreply.github.com>
2023-03-03 19:10:00 +0530

    Add files via upload
    15 Marks python coding Questions Asked in TCS IPA Exam in 25th Feb 2023

---
+++ 3rdMarch.py
@@ -0,0 +1,70 @@
+# Count the number of prime in the given list using sympy module
+
+"""
+from sympy import *
+
+n = int(input())
+l=[]
+for i in range(n):
+    l.append(int(input()))
+
+print(l)
+count = 0
+for i in l:
+    if(isprime(i) == True):
+        count +=1
+
+    else:
+        # print("notFound !")
+        pass
+
+print("Total Prime numbers in the list are = ",count)
+
+"""
+
+
+# Count the number of prime in the given list without using sympy module
+
+
+'''
+
+n = int(input())
+l = []
+for i in range(n):
+    l.append(int(input()))
+
+prime = []
+for i in range(0,len(l)):
+    for j in range(2,l[i]):
+        if l[i]%j==0:
+            # print("Not Prime")
+            pass
+            break
+    else:
+        prime.append(l[i])
+    count = len(prime)
+print(prime)
+print(count)
+
+'''
+
+# count the number of time a given word repeated in users input string 
+
+n = int(input())
+l=[]
+for i in range(n):
+    j = (input().split(" "))
+    l= l+j
+print(l)
+word = input()
+count = 0
+for i in l:
+    if i.lower() == word.lower():
+        count+=1
+if count!=0:
+    print("Count Of Given Word : ",count)
+else:
+    print("Not Found")
+
+
+    
