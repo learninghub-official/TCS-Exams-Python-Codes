16a2649a282378f78440ba67f3a10e7d3706b023
Mayank Nagar <71215472+learninghub-official@users.noreply.github.com>
2023-03-05 12:03:09 +0530

    Add files via upload
    35 Marks Question Asked in TCS IPA exam on 25th Feb 2023

---
+++ 5thMarch.py
@@ -0,0 +1,65 @@
+# 35 Marks Question Of TCS IPA held on 25 Feb 2023
+
+class Film :
+    def __init__(self,filmStudio , filmName,Price,Language):
+        self.filmStudio = filmStudio
+        self.filmName = filmName
+        self.price = Price
+        self.Language = Language
+    def search_print(self,list):
+        flag = 0
+        for i in list:
+            if i.Language.lower() == 'English'.lower() and i.price >= 500:
+                flag = 1
+                print(i.filmName , i.filmStudio)
+                
+        if flag==0:
+            print("Movie Not Found")
+        # else:
+
+
+n = int(input())
+list = []
+for i in range(n):
+    filmStudio = input()
+    filmName = input()
+    Price = int(input())
+    Language = input()
+
+    FilmObj = Film(filmStudio , filmName,Price,Language)
+    list.append(FilmObj)
+FilmObj.search_print(list)
+
+
+"""
+
+input 1:
+2
+MayankStudio
+Ironman
+200
+Hindi
+GavishStudio
+Thor
+300
+Hindi
+
+Output:
+Movie Not Found
+
+##########################
+ 
+intput 2:
+2
+MayankStudio
+Ironman
+6000
+english
+GavishStudio
+Thor
+300
+Hindi
+
+output:
+Ironman MayankStudio
+"""
