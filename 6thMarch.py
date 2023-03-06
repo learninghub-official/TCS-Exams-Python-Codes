c347900b5c64e93e33441cb18d4eaca3ee0215fd
Mayank Nagar <71215472+learninghub-official@users.noreply.github.com>
2023-03-06 16:35:50 +0530

    Add files via upload

---
+++ 6thMarch.py
@@ -0,0 +1,34 @@
+# Accenture Python Coding question 11 august 2021
+
+r = int(input())
+unit = int(input())
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+
+print("Numbers of Rats",r)
+print(f"Each Rat consume {unit} units :" )
+print(f"{n} Houses with units available : ",arr)
+
+totalunitrequired = unit*r
+# print(totalunitrequired)
+# print(arr)
+s =0 
+j = 0
+for i in arr:
+    if s>totalunitrequired:
+        # print(j)
+        break
+    else:
+        s+=i
+        j+=1
+
+
+print("Here is the count of houses : ",j)
+
+
+
+
+    
+
