7e73c2b991573b00911829836cbe548ea658bb7b
Mayank Nagar <71215472+learninghub-official@users.noreply.github.com>
2023-03-13 16:26:40 +0530

    Add files via upload

---
+++ 13thMarch.py
@@ -0,0 +1,46 @@
+
+# Wrong approach for this problem
+
+'''
+s = "Let's take LeetCode Contest"
+print(s)
+
+for i in s:
+    l = s.split()
+print(l)
+x = len(l)
+print(x)
+m = []
+for i in range(x):
+    for j in l:
+        lx =[]
+        for k in j:
+            lx.append(k)
+
+        lx.reverse()
+
+        m += lx
+    print(m)
+    final = ""
+    final = " ".join(m)
+
+    print(final)
+
+    break
+ '''   
+
+# right approach for this problem 
+
+
+s = "Let's take LeetCode Contest"
+print(s)
+l = s.split()
+l1 = []
+for i in l:
+    l1.append(i[::-1])
+# print(l1)
+ss = ""
+for i in l1:
+    ss+=' '+i
+print(ss)
+
