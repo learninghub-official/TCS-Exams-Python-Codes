5a8c75a15e13adbc0d05943012aa2b87aaaf0409
Mayank Nagar <71215472+learninghub-official@users.noreply.github.com>
2023-03-14 23:06:30 +0530

    Update 14thMarch_nightoutPractice.py

--- 14thMarch_nightoutPractice.py
+++ 14thMarch_nightoutPractice.py
@@ -24,7 +24,7 @@ The position of the searched string is:  2
 '''
 
 # Another Method for the same Problem
-
+'''
 n = int(input())
 # print(n)
 l = []
@@ -34,3 +34,50 @@ m = input()
 for i in range(0,len(l)):
     if l[i].lower() == m.lower():
         print("The position of the searched string is: ",i)
+'''
+
+
+'''
+
+n = int(input())
+l =[]
+for i in range(n):
+    l.append(input())
+for i in l:
+    if i == i[::-1]:
+        print(i)
+        
+'''
+
+
+
+'''
+Input:
+3
+malayalam
+radar
+nitish
+
+Output:
+
+'''
+
+
+
+def check(l):
+    l1 = []
+    for i in l:
+        if i == i[::-1]:
+            l1.append(i)
+    return l1
+
+n = int(input())
+l =[]
+for i in range(n):
+    l.append(input())
+
+if check(l):
+    for i in check(l):
+        print(i)
+else:
+    print("Not Found !")
