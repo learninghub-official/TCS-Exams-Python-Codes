e2f032b888f53f8ba966486e9e4ab038f4971230
Mayank Nagar <71215472+learninghub-official@users.noreply.github.com>
2023-03-14 23:29:18 +0530

    Update 14thMarch_nightoutPractice.py

--- 14thMarch_nightoutPractice.py
+++ 14thMarch_nightoutPractice.py
@@ -39,7 +39,6 @@ for i in range(0,len(l)):
 
 # Print All Palindrom Strings present in the list 
 
-
 '''
 
 n = int(input())
@@ -66,7 +65,7 @@ Output:
 '''
 
 
-
+'''
 def check(l):
     l1 = []
     for i in l:
@@ -84,3 +83,37 @@ if check(l):
         print(i)
 else:
     print("Not Found !")
+'''
+
+"""
+
+Create a function count_words() which takes a string as input and creates a dictionary with a word in the string as a key and its value as the number of times the word is repeated in the string. It should return the dictionary.
+eg: “hello hi hello world hello” 
+dict={hello:3,hi:1,word:1}
+
+Create another function max_accurance_word() which takes a string as input and returns the word which is occurring a maximum number of times in the string. Use the count_words function inside this function.
+
+"""
+
+
+
+
+def count_words(string):
+    l=string.split()
+    s=set(l)
+    d={}
+    for i in s:
+        x=l.count(i)
+        d[i]=x
+    return d
+def max_occurance(string):
+    d=count_words(string)
+    l1=[]
+    for i in d.values():
+        l1.append(i)
+    max1=max(l1)
+    for i in d.keys():
+        if d[i]==max1:
+            return i
+string=input() 
+print(max_occurance(string))
