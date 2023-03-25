12fcf3d2067fe9626742880a3efea8b18858d254
Mayank Nagar <71215472+learninghub-official@users.noreply.github.com>
2023-03-25 22:08:43 +0530

    Update 25thMarchNightOut.py

--- 25thMarchNightOut.py
+++ 25thMarchNightOut.py
@@ -36,3 +36,51 @@ Output:
 N should be greater than 3
 
 """ 
+
+'''
+class Movie:
+    def __init__(self,id,director,rating,budget):
+        self.id = id
+        self.director = director
+        self.rating = rating
+        self.budget = budget
+class Solution:
+    def __init__(self,list_movie):
+        self.list_movie = list_movie
+    def findAvgBudgetByDirector(self,user_director):
+        avg = 0
+        count = 0
+        for i in list_movie:
+            if i.director.lower() == user_director.lower():
+                count +=1
+                avg += i.budget
+        avg = avg/count
+        return avg
+    def getMoviebyratingandbudget(self,user_rating,user_budget):
+        for i in list_movie:
+            if i.rating == user_rating and i.budget == user_budget and i.budget%i.rating ==0:
+                # print(i)
+                print(i.id , i.director , i.rating , i.budget)
+        
+n = int(input())
+list_movie = []
+for i in range(n):
+    id = int(input())
+    director = input()
+    rating = int(input())
+    budget = int(input())
+    objMovie = Movie(id,director,rating,budget)
+    list_movie.append(objMovie)
+user_director = input()
+user_rating = int(input())
+user_budget = int(input())
+objSolution = Solution(list_movie)
+ans1 = objSolution.findAvgBudgetByDirector(user_director)
+if ans1:
+    print(ans1)
+else:
+    print("No movie of this director")
+ans2 = objSolution.getMoviebyratingandbudget(user_rating,user_budget)
+if ans2:
+    print(ans2)
+    '''
