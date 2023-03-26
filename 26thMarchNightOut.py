28cf81d5142916a5d2247ab8526568a8ecd29bce
Mayank Nagar <71215472+learninghub-official@users.noreply.github.com>
2023-03-26 22:35:22 +0530

    Update 26thMarchNightOut.py

--- 26thMarchNightOut.py
+++ 26thMarchNightOut.py
@@ -1,3 +1,5 @@
+'''
+
 class Doctor:
     def __init__(self,docId,name,spec,consultfee):
         self.docId = docId
@@ -45,7 +47,7 @@ else:
 
 
 
-
+'''
 '''
 Input:
 5
@@ -78,4 +80,85 @@ Output:
 90905 Madhuri Cardiologist 1000
 1900
 
+'''
+
+class laptop:
+    def __init__(self,id,brand,osType,price,rating):
+        self.id = id
+        self.brand = brand 
+        self.osType = osType
+        self.price = price
+        self.rating = rating
+class Solution:
+    def __init__(self,list):
+        self.list = list
+    def CountLaptop(slef,userBrand):
+        c = 0
+        for i in list:
+            if i.brand.lower()== userBrand.lower():
+                c+=1
+        return c
+    def SearchLaptopbyosType(self,userOs):
+        l1 = []
+        for i in list:
+            if i.osType.lower() == userOs.lower():
+                l1.append(i)
+            # l2 = l1.sort()
+        for j in l1:
+            print(f"{j.id} {j.rating}")
+            # print(j.id,j.rating)
+
+                
+list = []
+for i in range(int(input())):
+    id = int(input())
+    brand =  input()
+    osType = input()
+    price = float(input())
+    rating = int(input())
+    objLaptop = laptop(id,brand,osType,price,rating)
+    list.append(objLaptop)
+userBrand = input()
+userOs = input()
+objSol = Solution(list)
+ans1 = objSol.CountLaptop(userBrand)
+if ans1:
+    print(ans1)
+else:
+    print("No Laptop found")
+
+ans2 = objSol.SearchLaptopbyosType(userOs)
+
+
+
+'''
+Input:
+4
+123
+HP
+windows
+35000
+5
+124
+Apple
+Mac Os
+70000
+5
+125
+Dell
+Ubuntu
+30000
+4
+126 
+HP
+windows
+40000
+4
+HP
+
+Output:
+
+2
+123 5
+126 4
 '''
