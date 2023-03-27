664a6ee7b8553c205ae47506e3ded38e6a9e5fa5
Mayank Nagar <71215472+learninghub-official@users.noreply.github.com>
2023-03-28 00:53:36 +0530

    Update 27thMarchNightout.py

--- 27thMarchNightout.py
+++ 27thMarchNightout.py
@@ -1,49 +1,89 @@
-class vegitable:
-    def __init__(self,name,price,quantity):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-class Store:
+
+class Player:
+    def __init__(self,id,name,runs,playerType,matchType):
+        self.id = id 
+        self.name= name
+        self.runs = runs 
+        self.playerType= playerType
+        self.matchType = matchType
+class Solution:
     def __init__(self,list):
         self.list = list
-    def catrgorizevegialphabates(self,n):
+
+    def findPlayerWithLowestRun(self,userPlayerType):
+        l=[]
         for i in list:
-            if(i.price)>=n:
-                print("Expensive")
+            if i.playerType.lower() == userPlayerType.lower():
+                l.append(i.runs)
+            # m = min(l)
+        if l == []:
+            print("Not Found")
+        else:
+            m = min(l)
+            return m
+    def findPlayerbyMatchType(self,userMatchType):
+        l1 = []
+        for i in list:
+            if i.matchType.lower() == userMatchType.lower():
+                l1.append(i.id)
+            l1.sort()
+        return l1
 
 list = []
 for i in range(int(input())):
+    id = int(input())
     name = input()
-    price = float(input())
-    quantity = int(input())
-    objV = vegitable(name,price,quantity)
-    list.append(objV)
-n = int(input())
-objS = Store(list)
-ans1 = objS.catrgorizevegialphabates(n)
+    runs = int(input())
+    playerType  = input()
+    matchType = input()
+    objPlayers = Player(id,name,runs,playerType,matchType)
+    list.append(objPlayers)
+
+userPlayerType = input()
+userMatchType = input()
+objSol = Solution(list)
+ans1 = objSol.findPlayerWithLowestRun(userPlayerType)
+ans2 = objSol.findPlayerbyMatchType(userMatchType)
+if ans1:
+    print("Lowest Run Player :",ans1)
+else:
+    print("All are Equal")
+if ans2:
+    for i in ans2:
+        print(i)
+else:
+    print("Player Not Found")
+
+
+'''
+4
+11
+Sachin
+100
+International
+one Day
+12
+Shewag
+133
+International
+Test
+13 
+Varun
+78
+State
+Test
+14
+Ashwin
+67
+State
+One Day
+State
+One Day
+
+Output:
+Lowest Run Player : 67
+11
+14
 
 '''
 
-5
-corn
-20.0
-30
-onion
-10.0
-15
-potato
-30.0
-10
-cucumber
-5.0
-10
-brocolli
-24.5
-8
-5
-Expensive
-Expensive
-Expensive
-Expensive
-Expensive
-'''
